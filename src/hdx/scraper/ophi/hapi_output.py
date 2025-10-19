41fb41c06a774c726b302fcd75202c8358291291
Mike <rans@email.com>
2025-10-20 10:06:50 +1300

    No HAPI HXL (#6)

--- src/hdx/scraper/ophi/hapi_output.py
+++ src/hdx/scraper/ophi/hapi_output.py
@@ -4,7 +4,6 @@ from typing import Dict, List, Optional
 from hdx.api.configuration import Configuration
 from hdx.location.adminlevel import AdminLevel
 from hdx.location.country import Country
-from hdx.utilities.dictandlist import invert_dictionary
 
 logger = getLogger(__name__)
 
@@ -24,38 +23,37 @@ class HAPIOutput:
         self._rows = {}
 
     def create_rows(self, rows: Dict, dataset_id: str, resource_id: str) -> None:
-        hxltag_to_header = invert_dictionary(
-            self._configuration["hapi_dataset"]["resource"]["hxltags"]
-        )
-        input_to_output = {}
-        for header, hxltag in self._configuration["hxltags"].items():
-            input_to_output[header] = hxltag_to_header.get(hxltag)
-
         for row in rows.values():
             output_row = {}
-            for header in row:
-                output_header = input_to_output[header]
-                if output_header is not None:
-                    output_row[output_header] = row[header]
-            countryiso3 = output_row["location_code"]
+            countryiso3 = row["Country ISO3"]
+            output_row["location_code"] = countryiso3
             output_row["has_hrp"] = (
                 "Y" if Country.get_hrp_status_from_iso3(countryiso3) else "N"
             )
             output_row["in_gho"] = (
                 "Y" if Country.get_gho_status_from_iso3(countryiso3) else "N"
             )
-            provider_admin1_name = output_row["admin1_name"]
+            provider_admin1_name = row["Admin 1 Name"]
             output_row["provider_admin1_name"] = provider_admin1_name
-            admin1_code = output_row["admin1_code"]
+            admin1_code = row["Admin 1 PCode"]
             if admin1_code:
+                output_row["admin1_code"] = admin1_code
                 output_row["admin1_name"] = self._adminone.pcode_to_name[admin1_code]
                 output_row["admin_level"] = 1
             else:
+                output_row["admin1_code"] = ""
                 output_row["admin1_name"] = ""
                 if provider_admin1_name:
                     output_row["admin_level"] = 1
                 else:
                     output_row["admin_level"] = 0
+            output_row["mpi"] = row["MPI"]
+            output_row["headcount_ratio"] = row["Headcount Ratio"]
+            output_row["intensity_of_deprivation"] = row["Intensity of Deprivation"]
+            output_row["vulnerable_to_poverty"] = row["Vulnerable to Poverty"]
+            output_row["in_severe_poverty"] = row["In Severe Poverty"]
+            output_row["reference_period_start"] = row["Start Date"]
+            output_row["reference_period_end"] = row["End Date"]
             output_row["dataset_hdx_id"] = dataset_id
             output_row["resource_hdx_id"] = resource_id
             key = (
